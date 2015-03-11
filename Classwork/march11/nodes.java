b24e566fa856fe8fa60e939c0f7af8ce751e400a
Melissa <melissa.ye@cslab1-30>
2015-03-11 09:22:48 -0400

    m

---
+++ Classwork/march11/nodes.java
@@ -0,0 +1,29 @@
+public class nodes{
+    private String data;
+    private Node next;
+
+
+    public Node(String s){
+	data = s;
+    }
+    public void SetData(s){
+	data = s;
+    }
+    public String getData(){
+	return data;
+    }
+    public void setNext(Node n){
+	next = n;
+    }
+    public Node getNext(){
+	return next;
+    }
+    public String toString(){
+	reutrn data;
+    }
+    
+    
+
+
+
+}
